3f075c0ff11959f1411d0a45725a97c1f0ca428c
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2025-03-27 01:37:59 -0500

    npm init

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./modules/auth/pages/login/login.component";
+import { RegisterComponent } from "./modules/auth/pages/register/register.component";
+import { DashboardComponent } from "./modules/dashboard/pages/dashboard/dashboard.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [DashboardComponent, LoginComponent, RegisterComponent],
+  templateUrl: './app.component.html',
+
+})
+export class AppComponent {
+  title = 'FortunatoFront';
+}
